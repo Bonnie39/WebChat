4df5268ba58b95c3e9adeec5b850872e9ffcf401
Bonnie <73203899+Bonnie39@users.noreply.github.com>
2024-02-13 18:04:58 -0600

    Update script.firebase.js

--- script.firebase.js
+++ script.firebase.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
             signInAnonymously(auth)
             .then((userCredential) => {
                 const user = userCredential.user;
-                console.log("Anonymous user signed in:", user.uid);
+                //console.log("Anonymous user signed in:", user.uid);
             })
             .catch((error) => {
                 console.error("Error signing in anonymously:", error);
